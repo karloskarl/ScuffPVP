1c76edd555d68091b90a70ef84e832bea02548bd
Karl Jaagosild <jaagosildkarl@gmail.com>
2023-11-22 22:32:04 +0200

    changes

---
+++ src/main/java/com/scuffpvp/MyListener.java
@@ -0,0 +1,14 @@
+package com.scuffpvp;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class MyListener implements Listener {
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Bukkit.broadcastMessage("Welcome to the server!");
+        event.setJoinMessage("Welcome, " + event.getPlayer().getName() + "!");
+    }
+}
